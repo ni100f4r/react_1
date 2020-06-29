5965bfc1c82717399136a700771f11fd1c1cb02f
Niloofar <nili.moradi2719@gmail.com>
2020-06-29 16:17:13 +0430

    group_filter__btn style in ToDoList.js

--- src/ToDoList.js
+++ src/ToDoList.js
@@ -6,8 +6,29 @@ import className from 'classnames'
 export class ToDoList extends Component {
     state = {
         todos : [],
-        inputValue : ''
-    };
+        inputValue : '',
+        filter : 'All'
+    }
+
+    setFilter = (filterName) => {
+        this.setState({
+            filter: filterName,
+        }
+        );
+    }
+
+    getTodos = () => {
+        switch(this.state.filter) {
+            case 'All':
+                return this.state.todos;
+            case 'Dones':
+                return this.state.todos.filter(t => t.done);
+            case 'Un-Dones':
+                return this.state.todos.filter(t => !t.done);
+            default:
+                return this.state.todos;
+        }
+    }
 
     add = taskName => {
         this.setState(prevState => {
@@ -62,7 +83,7 @@ export class ToDoList extends Component {
                 <hr></hr>
                 <ul className={style.list_style}>
                     {
-                    this.state.todos.map((task, index) => (
+                    this.getTodos().map((task, index) => (
                     <li style={{display: "flex", flexDirection : "row", flexWrap: "wrap", alignContent: "space-around", width: "300px"}}>
                         <div>
                         <button className={className(style.eddit_btn, {testClass : false})} onClick={() => this.toggle(index)} style={{marginRight : "5px"}}>
@@ -79,6 +100,12 @@ export class ToDoList extends Component {
                 </ul>
                 <button onClick={this.handeleAddtoPress} className={style.add_btn}>{this.props.buttonName}</button>
                 <input className={style.input_style} value={this.state.inputValue} onChange={this.handeleInputChange} type="text"></input>
+                <div className={style.btn_group}>
+                    <p style={{marginBottom: "8px", marginTop: "35px", fontSize: "small", color: "rgb(56, 41, 56);"}}>Filters</p>
+                    <button onClick={() => this.setFilter('All')} className={style.filter_btn} style={{borderTopLeftRadius: "4px", borderBottomLeftRadius: "4px"}}>All</button>
+                    <button onClick={() => this.setFilter('Dones')} className={style.filter_btn}>Dones</button>
+                    <button onClick={() => this.setFilter('Un-Dones')} className={style.filter_btn} style={{borderTopRightRadius: "4px", borderBottomRightRadius: "4px"}}>Un-Dones</button>
+                </div>
             </div>
         );
     }
