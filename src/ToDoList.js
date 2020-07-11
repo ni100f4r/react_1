969573f05d0a1af4901334cc2b455624bc3afa51
Niloofar <nili.moradi2719@gmail.com>
2020-07-11 10:28:40 +0430

    first test

--- src/ToDoList.js
+++ src/ToDoList.js
@@ -1,121 +1,165 @@
-import React, {Component} from 'react';
-import style from './ToDoList.module.css'
-import TodoListItem from './TodoListItem'
+import React, { Component } from "react";
+import style from "./ToDoList.module.css";
+import TodoListItem from "./TodoListItem";
+import classNames from "classnames";
 
-
+// var classNames = require('classnames');
+let id = 0;
 export class ToDoList extends Component {
-    state = {
-        todos : [],
-        inputValue : '',
-                filter : 'All'
-    }
+  state = {
+    todos: [],
+    inputValue: "",
+    filter: "All",
+  };
 
-    setFilter = (filterName) => {
-        this.setState({
-            filter: filterName,
-        }
-        );
-    }
+  setFilter = (filterName) => {
+    this.setState({
+      filter: filterName,
+    });
+  };
 
-    getTodos = () => {
-        switch(this.state.filter) {
-            case 'All':
-                return this.state.todos;
-            case 'Dones':
-                return this.state.todos.filter(t => t.done);
-            case 'Un-Dones':
-                return this.state.todos.filter(t => !t.done);
-            default:
-                return this.state.todos;
-        }
+  getTodos = () => {
+    switch (this.state.filter) {
+      case "All":
+        return this.state.todos;
+      case "Dones":
+        return this.state.todos.filter((t) => t.done);
+      case "Un-Dones":
+        return this.state.todos.filter((t) => !t.done);
+      default:
+        return this.state.todos;
     }
+  };
 
-    add = taskName => {
-        this.setState(prevState => {
-            return {
-            todos : [...prevState.todos, {name : taskName, done: "false"}]
-            }
-        });
-    }
+  add = (taskName) => {
+    // id++;
+    this.setState((prevState) => {
+      return {
+        todos: [...prevState.todos, { name: taskName, done: "false", id: id++fi }],
+      };
+    });
+  };
 
-    handeleAddtoPress = () => {
-        if(this.state.inputValue) {
-            this.add(this.state.inputValue);
-            this.setState(
-                {
-                    inputValue : ''
-                }
-            );
-        }
+  handeleAddtoPress = () => {
+    if (this.state.inputValue) {
+      this.add(this.state.inputValue);
+      this.setState({
+        inputValue: "",
+      });
     }
+  };
 
-    handeleInputChange = event => {
-        this.setState(
-            {
-                inputValue : event.target.value,
-            }
-        );
-    }
+  handeleInputChange = (event) => {
+    this.setState({
+      inputValue: event.target.value,
+    });
+  };
 
-    delete = index => {
-        this.setState(prevState => {
-            return {
-            todos : [...prevState.todos.slice(0,index),...prevState.todos.slice(index+1)]
-            }
-        });
-    }
+  delete = (index) => {
+    this.setState((prevState) => {
+      return {
+        todos: [
+          ...prevState.todos.slice(0, index),
+          ...prevState.todos.slice(index + 1),
+        ],
+      };
+    });
+  };
 
-    toggle = index => {
-        this.setState(prevState => {
-            return {
-                todos : prevState.todos.map((task,i) => {
-                    if( i === index) return {...task, done : !task.done}
-                    else return task
-                })
-            }
-        });
-    }
+  toggle = (index) => {
+    this.setState((prevState) => {
+      return {
+        todos: prevState.todos.map((task, i) => {
+          if (i === index) return { ...task, done: !task.done };
+          else return task;
+        }),
+      };
+    });
+  };
 
-    edit = (newName, index) => {
-        this.setState(prevState => {
-            return {
-                todos: prevState.todos.map((task,i) => {
-                    if(i === index) return {...task, name: newName}
-                    else return task;
-                })
-            }
-        }
-        );
-    }
+  edit = (newName, index) => {
+    this.setState((prevState) => {
+      return {
+        todos: prevState.todos.map((task, i) => {
+          if (i === index) return { ...task, name: newName };
+          else return task;
+        }),
+      };
+    });
+  };
 
-    render() {
-        return(
-            <div className={style.ToDoList}>
-                <h2>To Do List</h2>
-                <hr></hr>
-                <ul className={style.list_style}>
-                    {
-                    this.getTodos().map((task, index) => (
-                        <TodoListItem {...task} 
-                        onToggle={()=> this.toggle(index)} 
-                        onDelete={()=> this.delete(index)}
-                        onEdit={(newName)=> this.edit(newName, index)}
-                        ></TodoListItem>
-                        )
-                    )
-                    }
-                </ul>
-                <button onClick={this.handeleAddtoPress} className={style.add_btn}>{this.props.buttonName}</button>
-                <input className={style.input_style} value={this.state.inputValue} onChange={this.handeleInputChange} type="text"></input>
-                <div className={style.btn_group}>
-                    <p style={{marginBottom: "8px", marginTop: "35px", fontSize: "small", color: "rgb(56, 41, 56);"}}>Filters</p>
-                    <button onClick={() => this.setFilter('All')} className={style.filter_btn} style={{borderTopLeftRadius: "4px", borderBottomLeftRadius: "4px"}}>All</button>
-                    <button onClick={() => this.setFilter('Dones')} className={style.filter_btn}>Dones</button>
-                    <button onClick={() => this.setFilter('Un-Dones')} className={style.filter_btn} style={{borderTopRightRadius: "4px", borderBottomRightRadius: "4px"}}>Un-Dones</button>
-                </div>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className={style.ToDoList}>
+        <h2>To Do List</h2>
+        <hr></hr>
+        <ul className={style.list_style}>
+          {this.getTodos().map((task, index) => (
+            <TodoListItem
+              {...task}
+              key={task.id}
+              onToggle={() => this.toggle(index)}
+              onDelete={() => this.delete(index)}
+              onEdit={(newName) => this.edit(newName, index)}
+            ></TodoListItem>
+          ))}
+        </ul>
+        <button onClick={this.handeleAddtoPress} className={style.add_btn}>
+          {this.props.buttonName}
+        </button>
+        <input
+          className={style.input_style}
+          value={this.state.inputValue}
+          onChange={this.handeleInputChange}
+          type="text"
+        ></input>
+        <div className={style.btn_group}>
+          <p
+            style={{
+              marginBottom: "8px",
+              marginTop: "35px",
+              fontSize: "small",
+              color: "rgb(56, 41, 56);",
+            }}
+          >
+            Filters
+          </p>
+          <button
+            onClick={() => this.setFilter("All")}
+            className={classNames(style.filter_btn, {
+              [style.active_btn]: this.state.filter === "All",
+            })}
+            style={{
+              borderTopLeftRadius: "4px",
+              borderBottomLeftRadius: "4px",
+            }}
+          >
+            All
+          </button>
+          <button
+            onClick={() => this.setFilter("Dones")}
+            className={classNames(style.filter_btn, {
+              [style.active_btn]: this.state.filter === "Dones",
+            })}
+          >
+            Dones
+          </button>
+          <button
+            onClick={() => this.setFilter("Un-Dones")}
+            className={classNames(style.filter_btn, {
+              [style.active_btn]: this.state.filter === "Un-Dones",
+            })}
+            style={{
+              borderTopRightRadius: "4px",
+              borderBottomRightRadius: "4px",
+            }}
+          >
+            Un-Dones
+          </button>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default ToDoList;
--- src/TodoListItem.js
+++ src/TodoListItem.js
@@ -1,31 +1,113 @@
-import React, { Component } from 'react';
-import style from './ToDoList.module.css'
-import className from 'classnames'
-
+import React, { Component } from "react";
+import style from "./ToDoList.module.css";
+import className from "classnames";
 
 class TodoListItem extends Component {
-    componentDidMount() {
-        this.props.onEdit('niloofar');
-    }
+  state = {
+    editing: false,
+    inputValue: "",
+  };
 
-    render() {
-                return(
+  handeldEditPressed = () => {
+    this.setState({
+      inputValue: this.props.name,
+      editing: true,
+    });
+  };
 
+  handeleInputChange = (event) => {
+    this.setState({
+      inputValue: event.target.value,
+    });
+  };
 
-                    
-            <li style={{display: "flex", flexDirection : "row", flexWrap: "wrap", alignContent: "space-around", width: "300px"}}>
-                <div>
-                    <button className={className(style.eddit_btn, {testClass : false})} onClick={this.props.onToggle} style={{marginRight : "5px"}}>
-                        {this.props.done ? 'Done' : 'Un-Done'}
-     </button>
-                        <button className={style.eddit_btn} style={{marginRight : "5px"}}>Edit</button>
-                        <button className={style.eddit_btn} onClick={this.props.onDelete} style={{marginRight : "5px"}}>Delete</button>
-                </div>
-                <div style={{marginLeft: "auto", textDecoration: this.props.done ? 'none' : 'line-through'}}>{this.props.name}</div>
-            </li>
-        )
-    }
+  handeldSaveEdit = () => {
+    this.props.onEdit(this.state.inputValue);
+    this.setState({
+      editing: false,
+    });
+  };
+
+  handeldCancelEdit = () => {
+    this.setState({
+      editing: false,
+      inputValue: "",
+    });
+  };
+
+  render() {
+    return (
+      <li
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          flexWrap: "wrap",
+          alignContent: "space-around",
+          width: "300px",
+        }}
+      >
+        {this.state.editing ? (
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "row",
+              margin: "10px 0px",
+            }}
+          >
+            <button
+              className={style.eddit_btn}
+              onClick={this.handeldCancelEdit}
+            >
+              Cancel
+            </button>
+            <button className={style.eddit_btn} onClick={this.handeldSaveEdit}>
+              Save
+            </button>
+            <input
+              className={style.input_style}
+              value={this.state.inputValue}
+              onChange={this.handeleInputChange}
+              type="text"
+            ></input>
+          </div>
+        ) : (
+          <>
+            <div>
+              <button
+                className={className(style.eddit_btn, { testClass: false })}
+                onClick={this.props.onToggle}
+                style={{ marginRight: "5px" }}
+              >
+                {this.props.done ? "Done" : "Un-Done"}
+              </button>
+              <button
+                className={style.eddit_btn}
+                onClick={this.handeldEditPressed}
+                style={{ marginRight: "5px" }}
+              >
+                Edit
+              </button>
+              <button
+                className={style.eddit_btn}
+                onClick={this.props.onDelete}
+                style={{ marginRight: "5px" }}
+              >
+                Delete
+              </button>
+            </div>
+            <div
+              style={{
+                marginLeft: "auto",
+                textDecoration: this.props.done ? "none" : "line-through",
+              }}
+            >
+              {this.props.name}
+            </div>
+          </>
+        )}
+      </li>
+    );
+  }
 }
 
 export default TodoListItem;
-
